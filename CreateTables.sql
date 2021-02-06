27f6a89fbc1eca98df42ed8f92d2c881ddbbc5eb
DanTolman11 <57876706+DanTolman11@users.noreply.github.com>
2021-02-06 18:20:14 +0000

    Updated the primary keys
    The creation of the tables was updated to create two primary keys

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,24 +1,25 @@
 CREATE TABLE dbo.users
 (
-    userId INT NOT NULL,
+    userId INT IDENTITY (1,1) NOT NULL,
     firstName VARCHAR(35) NOT NULL,
     lastName VARCHAR(35) NOT NULL,
     Email VARCHAR(60) NOT NULL,
-    Password VARCHAR(35) NOT NULL,
+    uesrPassword VARCHAR(35) NOT NULL,
 
     CONSTRAINT pkUserId PRIMARY KEY(userId),
 ); 
 
 CREATE TABLE dbo.password
 (
-    passwordId INT,
+    passwordId INT IDENTITY(1,1),
     previousPassword VARCHAR(35),
     changeDate DATETIME,
 );
 
 CREATE TABLE dbo.session
 (
-    sessionId INT NOT NULL,
+    sessionId INT IDENTITY(1,1) NOT NULL,
     sessionDate DATE NOT NULL,
     sessionTime DATETIME,
-);
+); 
+
